9d557372ef8855463f6d6d4f51a1d639eac81178
Arthur Tavares <arth_tavares@hotmail.com>
2020-02-01 15:09:58 -0300

    FIxe quicky minigame

--- Fixar/Assets/Scripts/Quicky.cs
+++ Fixar/Assets/Scripts/Quicky.cs
@@ -4,33 +4,29 @@ public class Quicky : MiniGameBase
 {
     int  key,prevkey,damage=10;
     SpriteRenderer renderer;
-    Vector3[] pos={new Vector3(0,5),new Vector3(-5,0),new Vector3(5,0),new Vector3(0,-5)};
     public override void Create(){
-        objects=new GameObject[5];
-        for (int i = 0; i < 4; i++)
-        {
-            objects[i]=new GameObject("arrow");
-            objects[i].AddComponent<SpriteRenderer>().sprite=sprites[i];
-            objects[i].transform.position=pos[i];
-            objects[i].SetActive(false);
-        }
-        objects[4]=new GameObject("building");
-        renderer=objects[4].AddComponent<SpriteRenderer>();
-        renderer.sprite=sprites[5];
+        objects=new GameObject[2];
+        
+        objects[0]=new GameObject("arrow");
+        objects[0].AddComponent<SpriteRenderer>().sprite=sprites[0];
+        
+        objects[1]=new GameObject("building");
+        renderer=objects[1].AddComponent<SpriteRenderer>();
+        renderer.sprite=sprites[1];
         prevkey=key=Random.Range(0,4);
+        objects[0].transform.rotation=Quaternion.Euler(0,0,key*-90);
     }
     public override void Update()
     {
-        objects[key].SetActive(true);
         if(KeyDown()){
             damage--;
-            renderer.sprite=sprites[4+((10-damage)/5)];
+            renderer.sprite=sprites[1+((10-damage)/5)];
             if(damage==0)End(State.won);
             else{
-                objects[key].SetActive(false);
                 int i=key;
                 do key=Random.Range(0,4);
                 while(key==prevkey);
+                objects[0].transform.rotation=Quaternion.Euler(0,0,key*-90);
                 prevkey=i;
             }
         }
@@ -44,9 +40,9 @@ public class Quicky : MiniGameBase
             case 1:
                 return Input.GetKeyDown(KeyCode.LeftArrow);
             case 2:
-                return Input.GetKeyDown(KeyCode.RightArrow);
-            case 3:
                 return Input.GetKeyDown(KeyCode.DownArrow);
+            case 3:
+                return Input.GetKeyDown(KeyCode.RightArrow);
             default:
                 return false;
         }
