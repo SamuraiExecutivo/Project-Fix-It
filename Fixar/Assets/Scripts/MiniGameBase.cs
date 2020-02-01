8b68834d41934f2fed5b3a20f5cb504547efcc78
Arthur Tavares <arth_tavares@hotmail.com>
2020-02-01 01:52:36 -0300

    Added some basic script for mini games functionality

---
+++ Fixar/Assets/Scripts/MiniGameBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class MiniGameBase
+{
+    public AudioClip[] notes;
+    public Sprite[] sprites;
+    public bool ended{private set; get;}
+    protected GameObject[] objects;
+    public virtual void Create(){}
+    public virtual void Load(){}
+    public virtual void Update(){}
+    public void End(){
+        foreach (GameObject go in objects)
+        {
+            Object.Destroy(go);
+        }
+        ended=true;
+    }
+}
