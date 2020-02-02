1846d03b26899b1743e47ba9eed5fe7af36abae2
Yuri Katayama <yurikbcarvalho@gmail.com>
2020-02-02 10:15:04 -0200

    Revert "??"
    This reverts commit c442adbd0100faf7c58193b21deaf0fe54ffa665, reversing
    changes made to 948ff3f0eefb5b8f614c8f8c37f228a72e26ac4c.

--- Fixar/Assets/Scripts/MiniGameBase.cs
+++ Fixar/Assets/Scripts/MiniGameBase.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 [System.Serializable]
 public class MiniGameBase {
 
-    public enum State {
+    public enum State{
         running,
         won,
         lost
-
+        
     }
     public State state;
     public AudioClip[] notes;
     public Sprite[] sprites;
-    public bool ended { get { return state != State.running; } }
-    public float timer { protected set; get; } = 5;
+    public bool ended {get{return state!=State.running;}}
+    public float timer{protected set;get;}=5;
     protected GameObject[] objects;
     public virtual void Create () { }
     public virtual void Load () { }
     public virtual void Update () { }
-    protected virtual void Timer () {
-        timer -= Time.deltaTime;
-        if (timer <= 0) End (State.lost);
+    protected virtual void Timer(){
+        timer-=Time.deltaTime;
+        if(timer<=0)End(State.lost);
     }
     public void End (State s) {
-        state = s;
+        state=s;
     }
-    public void Clear () {
+    public void Clear(){
         foreach (GameObject go in objects) {
             Object.Destroy (go);
         }
