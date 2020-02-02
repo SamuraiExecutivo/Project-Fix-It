86c6cf80b8c1dc75099be0099d987021c003ef43
Yuri Katayama <yurikbcarvalho@gmail.com>
2020-02-02 12:37:57 -0200

    Game Flow

--- Fixar/Assets/Scripts/GamePlayRunner.cs
+++ Fixar/Assets/Scripts/GamePlayRunner.cs
@@ -1,44 +1,49 @@
-﻿using UnityEngine.UI;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class GamePlayRunner : MonoBehaviour
-{
+public class GamePlayRunner : MonoBehaviour {
+    
     public MiniGameInfo[] miniGames;
+    public MiniGameInfo mainGame;
     MiniGameBase chosen;
     public Slider slider;
     public Text counter;
-    public Image win,lose;
+    public Image win, lose;
     float timer;
     int id;
-    void Start()
-    {
-        Chose();
-    }
-    void Chose(){
-        chosen?.Clear();
-        int i=id;
-        do id=Random.Range(0,miniGames.Length);
-        while(id==i);
-        chosen=miniGames[id].GetMiniGame();
-        chosen.Create();
+
+    void Start () {
+        chosen = mainGame.GetMiniGame ();
+        chosen.Create ();
         slider.value = slider.maxValue = chosen.timer;
-        win.enabled=lose.enabled=false;
-        timer=2;
+        win.enabled = lose.enabled = false;
+        timer = 2;
     }
-    void Update()
-    {
-        if(chosen!=null && !chosen.ended){
-            chosen.Update();
-            slider.value=chosen.timer;
-            counter.text=(Mathf.RoundToInt(slider.value*100)/100f).ToString();
-        }
-        else{
-            timer-=Time.deltaTime;
-            win.enabled=chosen.state==MiniGameBase.State.won;
-            lose.enabled=chosen.state==MiniGameBase.State.lost;
-            slider.gameObject.SetActive(timer<=0);
-            if(timer<=0){
-                Chose();
+
+    void Chose () {
+        chosen?.Clear ();
+        int i = id;
+        do id = Random.Range (0, miniGames.Length);
+        while (id == i);
+        chosen = miniGames[id].GetMiniGame ();
+        chosen.Create ();
+        slider.value = slider.maxValue = chosen.timer;
+        win.enabled = lose.enabled = false;
+        timer = 2;
+    }
+
+    void Update () {
+        if (chosen != null && !chosen.ended) {
+            chosen.Update ();
+            slider.value = chosen.timer;
+            counter.text = (Mathf.RoundToInt (slider.value * 100) / 100f).ToString ();
+        } else {
+            timer -= Time.deltaTime;
+            win.enabled = chosen.state == MiniGameBase.State.won;
+            lose.enabled = chosen.state == MiniGameBase.State.lost;
+            slider.gameObject.SetActive (timer <= 0);
+            if (timer <= 0) {
+                Chose ();
             }
         }
     }
